77d9f58fd2c6bc855934c65fc81bfa6d66133358
Nick Sonner <nicksonner@gmail.com>
2018-04-17 12:35:37 +1000

    added ball movement, added code to remove each prior rendered ball

--- code/app-0.js
+++ code/app-0.js
@@ -3,6 +3,8 @@ var ctx = canvas.getContext('2d')
 
 var x = canvas.width / 2
 var y = canvas.height - 30
+var deltaX = 2
+var deltaY = -2
 var ballRadius = 10
 
 function drawBall() {
@@ -14,10 +16,18 @@ function drawBall() {
 }
 
 function draw() {
+  // clears the canvas after the ball is drawn, thus removing each proir ball that is drawn
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  
   // draw ball
   drawBall()
-  
+
+  // move the position of the ball, by changing the value of the balls origin point
+  x += deltaX
+  y += deltaY
+  // this becomes recursive, so the game state/canvas is refreshed constantly
   requestAnimationFrame(draw)
 }
 
+// calls for an animation frame to be drawn using a callback (draw)
 requestAnimationFrame(draw)
